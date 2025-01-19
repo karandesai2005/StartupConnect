0dc33992177a9e870d99999a2f9619a6da1c7e8c
Bhoomika DJ <bhoomsdj@gmail.com>
2025-01-19 22:08:31 +0530

    pullandpush

--- App.js
+++ App.js
@@ -6,7 +6,11 @@ import LoginScreen from './components/LoginScreen';
 import HomeScreen from './components/Home';
 import ProfileScreen from './components/profile';
 import ChatScreen from './components/Chat';
+import Register1 from "./components/Register1"; // Import Register1
+import Register2 from "./components/Register2"; // Import Register1
 import EditProfileP from './components/editProfileP';
+
+
 
 
 const Stack = createStackNavigator();
@@ -20,11 +24,13 @@ const App = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen}/>
         <Stack.Screen name="Chat" component={ChatScreen}/>
+        <Stack.Screen name="Register1" component={Register1}/>
+        <Stack.Screen name="Register2" component={Register2}/>
         <Stack.Screen name="EditProfileP" component={EditProfileP}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
