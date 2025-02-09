16046be212904dea290b138438980dce4f4994d1
karandesai2005 <karanishudesai2@gmail.com>
2025-02-09 19:44:16 +0530

    Updated backend files

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./.env" });  // Load backend .env
+require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
 const app = require("./app");
 const path = require("path");
