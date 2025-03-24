aa067c21842c0564d437bbe6af6168e1f03c4bbf
Bhoomika DJ <bhoomsdj@gmail.com>
2025-03-24 19:21:35 +0530

    del in profile

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
+  StatusBar,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
