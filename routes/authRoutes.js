09210eb84bb66581dcc1c439583e1cc3bb6a9b5f
karandesai2005 <karanishudesai2@gmail.com>
2025-04-15 06:12:24 +0530

    final

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -56,8 +56,4 @@ router.route('/posts/user/:username')
 
 router.route('/search-users')
   .get(authenticateJWT, searchUsers);
-
-// POSSIBLE STRAY CODE CAUSING ERROR (LINE 45?)
-router.get('/some-route', undefinedFunction); // This would trigger the error
-
 module.exports = router;
